ea33f4ea6c05256ca9649c6ee37297b3b392a1b7
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2024-12-16 19:44:20 +0000

    Criando a rota de login

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-require('dotenv').config({})
+require('dotenv').config()
 const express = require('express')
-const router = require('./routes')
+const authRouter = require('./routes/auth')
+
 const app = express()
 
 app.use(express.json())
 
-app.use(router)
+app.use('/auth', authRouter)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Servidor iniciado em https://localhost:${PORT}/`))
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
--- src/models/users-model.js
+++ src/models/users-model.js
@@ -1,26 +1,26 @@
 const uuid = require('uuid').v4
+const bcrypt = require('bcrypt')
 
 const users = [
-    {id: '1', name: 'Felipe Suguiura', email: 'felipesuguiura.pro@gmail.com', password: '123456'},
-    {id: '2', name: 'John Wick', email: 'johnwick.pro@gmail.com', password: '654321'}
+  { id: '1', name: 'Felipe Suguiura', email: 'felipe@email.com', password: '123456' },
+  { id: '2', name: 'John Wick', email: 'john@email.com', password: '0000' }
 ]
 
 module.exports = {
-    getAllUsers: () => users,
+  getAllUsers: () => users,
 
-    getUserById: (id) => users.find(user => user.id === id),
+  getUserById: (id) => users.find(user => user.id === id),
 
-    getUserByEmail: (email) => users.find(user => user.email === email),
+  getUserByEmail: (email) => users.find(user => user.email === email),
 
-    createUser: (name, email, password) =>{
-        const newUser = {
-            id: uuid(),
-            name,
-            email,
-            password
-        }
-        
-        users.push(newUser)
-        return newUser
+  createUser: (name, email, password) => {
+    const newUser = {
+      id: uuid(),
+      name,
+      email,
+      password: bcrypt.hashSync(password, 10)
     }
+    users.push(newUser)
+    return newUser
+  }
 }
